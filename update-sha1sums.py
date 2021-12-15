6740294db251ca23003e2565f4daf4cf936a1538
Sourcery AI <>
2021-12-15 19:30:46 +0000

    'Refactored by Sourcery'

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -22,7 +22,7 @@ import sys
 device='phoenix'
 vendor='xiaomi'
 
-lines = [ line for line in open('proprietary-files.txt', 'r') ]
+lines = list(open('proprietary-files.txt', 'r'))
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
 needSHA1 = False
 
